00ee40575491cafdb3d1e29a763af421383abda2
shivamdeshwal14 <shivamdeshwal1414@gmail.com>
2025-07-28 18:55:26 +0530

    Intializing repo

---
+++ Src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+const App=()=>{
+    return <h1>Hello i am shivam deshwal</h1>
+}
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>)
